a4324e4ff488f3b32838eece86ca2bc4268dffb2
jsobie3 <jakub.sobieraj01@gmail.com>
2022-12-08 21:50:25 -0600

    2nd commit

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function header () {
+    return (
+        <div>
+            <ul id = ' nav' className = 'nav'>
+                <li className='navLink'>Test 1 </li>
+                <li className='navLink'>Test 2 </li>
+                <li className='navLink'>Test 3 </li>
+                <li className='navLink'>Test 4 </li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default header;
---
+++ src/components/portfoliocontainer.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+import Header from './Header'
+import Footer from './Footer'
+
+
+
+
+export default function PortfolioContainer() 
+  
